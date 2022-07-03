0c21e6b389b60cc9e4a69f5b3dfb19cee785eee6
Suha <serenab275@gmail.com>
2022-07-03 15:07:28 -0400

    Initial Commit

---
+++ TrexisProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrexisProject
+//
+//  Created by Suha Baobaid on 7/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TrexisProjectUITests/TrexisProjectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TrexisProjectUITestsLaunchTests.swift
+//  TrexisProjectUITests
+//
+//  Created by Suha Baobaid on 7/3/22.
+//
+
+import XCTest
+
+class TrexisProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
